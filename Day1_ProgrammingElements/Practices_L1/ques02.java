7de73e6802615b7d88f87c27a5db9947cd122a25
Bharat <bharatsinghchaudhary3@gmail.com>
2025-06-12 11:36:29 +0530

    Add Day1_ProgrammingElements folder to Core-Java branch

---
+++ Day1_ProgrammingElements/Practices_L1/ques02.java
@@ -0,0 +1,9 @@
+public class ques02 {
+    public static void main(String[] args) {
+        byte math = 94;
+        byte physics = 95;
+        byte chemistry = 96;
+        double average = (math + physics + chemistry)*100 / 300.0;
+        System.out.println("Harry's average marks in PCM is: " + average);
+    }
+}
---
+++ Day1_ProgrammingElements/Practices_L1/ques03.java
@@ -0,0 +1,7 @@
+public class ques03 {
+    public static void main(String[] args) {
+        double dis_km = 10.8;
+        double dis_miles = dis_km * (1/1.6);
+        System.out.println("The distance 10.8 km in miles is: " + dis_miles);
+    }
+}
